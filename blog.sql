901c39776aa1c19ea590eab7163f345a88545eb9
LunaTheGoddess <160146944+LunaTheGoddess@users.noreply.github.com>
2024-08-26 03:04:51 +0630

    Add files via upload

--- blog.sql
+++ blog.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Aug 19, 2024 at 08:34 PM
+-- Generation Time: Aug 25, 2024 at 10:34 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -24,6 +24,33 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `comment`
+--
+
+CREATE TABLE `comment` (
+  `cmd_id` int(11) NOT NULL,
+  `content` text NOT NULL,
+  `user_id` int(11) UNSIGNED NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `comment`
+--
+
+INSERT INTO `comment` (`cmd_id`, `content`, `user_id`) VALUES
+(1, 'Hi', 7),
+(2, 'Hey', 7),
+(3, 'hey', 8),
+(4, 'hey', 7),
+(7, 'i am good thank you', 16),
+(10, 'what&#039;s up?', 7),
+(11, 'Heyyyyyyyyyyyyyyyyy\r\n\r\nFellasssssssssss!!', 7),
+(12, 'yo', 7),
+(14, 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry&#039;s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, ', 7);
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `users`
 --
 
@@ -35,20 +62,29 @@ CREATE TABLE `users` (
   `email` varchar(100) NOT NULL,
   `password` varchar(255) NOT NULL,
   `avatar` varchar(255) NOT NULL,
-  `is_admin` tinyint(1) NOT NULL
+  `is_admin` tinyint(1) NOT NULL,
+  `bio` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `users`
 --
 
-INSERT INTO `users` (`id`, `firstname`, `lastname`, `username`, `email`, `password`, `avatar`, `is_admin`) VALUES
-(7, 'Eaint', 'Pyae Phyo', 'Luna', 'eaintpyaephyo.epp@gmail.com', '$2y$10$EpNNwKJnvelpxNG9TTAGvu85jgoTHBz.ea5wckK4uyekW3FPwYjIG', '1723979142tensura.jpg', 1),
-(8, 'Someone', 'in the World', 'real', 'iamreal@gmail.com', '$2y$10$erFuAj5V7y1tp8R/8YS0xObobIacoJD1HDM6FZLiHUcePmC6Q51fy', '1724003536Arlechinno.jpg', 1);
+INSERT INTO `users` (`id`, `firstname`, `lastname`, `username`, `email`, `password`, `avatar`, `is_admin`, `bio`) VALUES
+(7, 'Eaint', 'Pyae Phyo', 'Luna', 'eaintpyaephyo.epp@gmail.com', '$2y$10$EpNNwKJnvelpxNG9TTAGvu85jgoTHBz.ea5wckK4uyekW3FPwYjIG', '1724569424tensura.jpg', 1, 'I am just a programmer.'),
+(8, 'Someone', 'in the World', 'real', 'iamreal@gmail.com', '$2y$10$erFuAj5V7y1tp8R/8YS0xObobIacoJD1HDM6FZLiHUcePmC6Q51fy', '1724003536Arlechinno.jpg', 0, ''),
+(16, 'ab', 'b', 'c', 'f@d.v', '$2y$10$PcOL7HytBeNmB44d0kkloe2C.KTP1bdDYGpHOKqXNBkhazwwWsf3C', '1724511345BlogProject_ClassDiagram.png', 0, '');
 
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `comment`
+--
+ALTER TABLE `comment`
+  ADD PRIMARY KEY (`cmd_id`),
+  ADD KEY `user_id` (`user_id`);
 
 --
 -- Indexes for table `users`
@@ -61,10 +97,26 @@ ALTER TABLE `users`
 --
 
 --
+-- AUTO_INCREMENT for table `comment`
+--
+ALTER TABLE `comment`
+  MODIFY `cmd_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
+
+--
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `comment`
+--
+ALTER TABLE `comment`
+  ADD CONSTRAINT `comment_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
