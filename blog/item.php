b38cbf6c9308c29d3c0e3105f69a96ba5dc9e259
SuSuHlaing11 <115403289+SuSuHlaing11@users.noreply.github.com>
2024-08-28 15:06:42 +0630

    Update item.php

--- blog/item.php
+++ blog/item.php
@@ -1,46 +1,70 @@
 <?php
 include 'partials/header.php';
+
+if (isset($_SESSION['user-id'])) {
+    $user_id = $_SESSION['user-id'];
+} else {
+    // If not logged in, redirect to login page
+    header('Location: login.php');
+    exit();
+}
+
+
+if (isset($_GET['id'])) {
+    $id = filter_var($_GET['id'], FILTER_SANITIZE_NUMBER_INT);
+
+    // Fetch item details from the database
+    $query = "SELECT * FROM items WHERE id = $id";
+    $result = mysqli_query($connection, $query);
+    $item = mysqli_fetch_assoc($result);
+
+    if ($item) {
+        // Item found
+        $title = $item['title'];
+        $description = $item['description'];
+        $price = $item['price'];
+        $thumbnail = $item['thumbnail'];
+    } else {
+        echo "<p>Item not found.</p>";
+        exit;
+    }
+} else {
+    echo "<p>No item ID provided.</p>";
+    exit;
+}
 ?>
 
-
-        <section class="singlepost">
-            <div class="container singlepost__container">
-                <h2>This is the item</h2>
-                <div class="post__author">
-                <div class="post__author-avatar">
-                    <img src="./images/avatar2.jpg">
-                </div>
-                <div class="post__author-info">
-                        <h5>By: Jane Doe</h5>
-                        <small>July 01, 2024 - 07:23</small>
-                </div>
-            </div>
-            <div class="singlepost__thumbnail">
-                <img src="./images/item1.jpg">
-            </div>
-            <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Et repellat labore, officiis nam reprehenderit doloremque ullam! Placeat hic, soluta dolore, molestiae est quis blanditiis debitis aut illum, et atque eveniet!
-                Lorem ipsum dolor sit, amet consectetur adipisicing elit. In doloribus quidem veritatis. Saepe, dolorem id error similique ex dignissimos quidem tenetur omnis? Magni neque quae omnis, illo tenetur expedita eos.
-            </p>
-            <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Et repellat labore, officiis nam reprehenderit doloremque ullam! Placeat hic, soluta dolore, molestiae est quis blanditiis debitis aut illum, et atque eveniet!
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates harum est delectus quis ipsam itaque fugit? Sit harum quidem voluptatibus, pariatur, consectetur minus ullam unde molestiae beatae vitae distinctio et!
-            </p>
-            <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Et repellat labore, officiis nam reprehenderit doloremque ullam! Placeat hic, soluta dolore, molestiae est quis blanditiis debitis aut illum, et atque eveniet!
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga inventore sed quo culpa doloribus facere minima voluptatum. Neque delectus vel est maiores. Necessitatibus tenetur ut veniam doloribus inventore. Magni, tenetur?
-            </p>
-            <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Et repellat labore, officiis nam reprehenderit doloremque ullam! Placeat hic, soluta dolore, molestiae est quis blanditiis debitis aut illum, et atque eveniet!
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas quis molestiae eius illum rerum iusto culpa, magnam numquam ad? Quo itaque officia rerum? Nulla tenetur adipisci ipsa eius atque in.
-            </p>
-            <p></p>
-            <a class="buyit__button">Price : $123</a>
-            <a href="payment.php" class="buyit__button">Buy It</a>
+<section class="singlepost">
+    <div class="container singlepost__container">
+        <!-- title -->
+        <h2><?= htmlspecialchars($title) ?></h2>
+        <!-- thumbnail -->
+        <div class="singlepost__thumbnail">
+            <img src="./images/<?= htmlspecialchars($thumbnail) ?>" alt="Item Thumbnail">
         </div>
-    </section>
-<!--==============END OF SINGLE POST-->
+        <!-- description and price in a single line -->
+        <div>
+            <!-- description -->
+            <p class="singlepost__description">
+                <?= htmlspecialchars($description) ?>
+            </p>
+            <!-- price -->
+            <div style="display: flex; flex-direction:row;">
+                <button style="margin-right: 10px;" class="buyit__button">Price: $<?= htmlspecialchars($price) ?></button>
+                <!-- Form to handle the purchase -->
+                <form action="payment.php" method="get">
+                    <input type="hidden" name="id" value="<?= htmlspecialchars($id) ?>">
+                    <input type="hidden" name="title" value="<?= htmlspecialchars($title) ?>">
+                    <input type="hidden" name="price" value="<?= htmlspecialchars($price) ?>">
+                    <input type="hidden" name="user_id" value="<?= htmlspecialchars($user_id) ?>">
+                    <button type="submit" class="buyit__button">Buy It</button>
+                </form>
+            </div>
+        </div>
+    </div>
+</section>
 
+<!--==============END OF SINGLE POST-->
 
 <?php
 include 'partials/footer.php';
